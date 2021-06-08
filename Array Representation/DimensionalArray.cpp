4eeaa05fc4ead3bbe9a770032a2e1da3919c6062
JAIMIT PANCHAL <jaimitpanchal@JAIMITs-MacBook-Air.local>
2021-06-08 16:44:34 +0530

    pointer array with array

--- Array Representation/DimensionalArray.cpp
+++ Array Representation/DimensionalArray.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 
- int main(){
+int main()
+{
 
+    int Arr[3][4] = {{1, 2, 3, 75}, {2, 3, 4, 9}, {5, 6, 78}};
+    //in stack
 
-     return 0;
- }
+    int *B[3];
+    B[0] = (int *)malloc(4 * sizeof(int));
+    B[1] = (int *)malloc(4 * sizeof(int));
+    B[2] = (int *)malloc(4 * sizeof(int));
+    //2D Array in Heap
+
+    int **C;
+    //Assigning space for pointer Array in heap memory
+    C = (int **)malloc(3 * sizeof(int));
+
+    //Assigning each memory block an Array
+    C[0] = (int *)malloc(4 * sizeof(int));
+    C[1] = (int *)malloc(4 * sizeof(int));
+    C[2] = (int *)malloc(4 * sizeof(int));
+
+    return 0;
+}
