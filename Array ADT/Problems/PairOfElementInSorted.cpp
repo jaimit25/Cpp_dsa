2bc25a979e03c42264a513cd6e2098d075ff1d1a
JAIMIT PANCHAL <jaimitpanchal@JAIMITs-MacBook-Air.local>
2021-06-17 17:28:36 +0530

    update previous

--- Array ADT/Problems/PairOfElementInSorted.cpp
+++ Array ADT/Problems/PairOfElementInSorted.cpp
@@ -5,17 +5,41 @@ using namespace std;
 int main()
 {
 
-	int A[10] = {1,3,4,5,6,8,9,10,12,14};
+	int A[10] = {1, 3, 4, 5, 6, 8, 9, 10, 12, 14};
 	int k = 10;
-	int i =0, j = 9;
-	while(i < j ){
-		if(A[i]+A[j] == k ){
-			cout<<A[i]<< " + "<<A[j]<< " = "<<k<<endl;
+	int i = 0, j = 9;
+	while (i < j)
+	{
+		if (A[i] + A[j] == k)
+		{
+			cout << A[i] << " + " << A[j] << " = " << k << endl;
 			i++;
 			j--;
-		}else if(A[i] + A[j] < k){
+		}
+		else if (A[i] + A[j] < k)
+		{
 			i++;
-		}else{
+		}
+		else
+		{
+			j--;
+		}
+	}
+
+	for (int i = 0, j = 9; i < j;)
+	{
+		if (A[i] + A[j] == k)
+		{
+			cout << A[i] << " + " << A[j] << " = " << k << endl;
+			i++;
+			j--;
+		}
+		else if (A[i] + A[j] < k)
+		{
+			i++;
+		}
+		else
+		{
 			j--;
 		}
 	}
